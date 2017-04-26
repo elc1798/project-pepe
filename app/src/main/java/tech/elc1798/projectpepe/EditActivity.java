fbe76226b6f2a1e666afe8597e223146ba6b7716
elc1798 <elc1798@gmail.com>
2017-04-26 00:31:04 -0500

    Add edit activity layout

---
+++ app/src/main/java/tech/elc1798/projectpepe/EditActivity.java
@@ -0,0 +1,13 @@
+package tech.elc1798.projectpepe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_activity_layout);
+    }
+}
